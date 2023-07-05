5d00802cad33fc70191da3d9577dca12d9289575
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2023-07-05 15:47:27 +0100

    standalone: implement isModulePermissionSupported, upgradePermissions

--- CRM/Core/Permission/Standalone.php
+++ CRM/Core/Permission/Standalone.php
@@ -52,4 +52,40 @@ class CRM_Core_Permission_Standalone extends CRM_Core_Permission_Base {
     return isset($this->permissions) && is_array($this->permissions) ? in_array($str, $this->permissions) : TRUE;
   }
 
+  /**
+   * Determine whether the permission store allows us to store
+   * a list of permissions generated dynamically (eg by
+   * hook_civicrm_permissions.)
+   *
+   * @return bool
+   */
+  public function isModulePermissionSupported() {
+    return TRUE;
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function upgradePermissions($permissions) {
+    // Standalone does not store a list of permissions, so we don't need to
+    // do anything with new permissions. But if any of our users have a
+    // permission that no longer exists, we need to remove that now.
+    $roles = \Civi\Api4\Role::get(FALSE)
+    ->execute();
+    $validPermissions = array_keys($permissions);
+    $records = [];
+    $rolesAffected = [];
+    foreach ($roles as $role) {
+      $newPermissions = array_intersect($role['permissions'], $validPermissions);
+      if ($newPermissions !== $role['permissions']) {
+        $records[] = ['id' => $role['id'], 'permissions' => $newPermissions];
+        $rolesAffected[] = $role['name'];
+      }
+    }
+    if ($records) {
+      \Civi\Api4\Role::save(FALSE)->setRecords($records)->execute();
+      \Civi::log()->info("Removed old permissions from roles: " . implode(', ', $rolesAffected));
+    }
+  }
+
 }
